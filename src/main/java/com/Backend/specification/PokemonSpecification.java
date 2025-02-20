e6a8aefda5822586c9912f3af75f289d106964b0
JulianBlanco98 <jblancoho@alumnos.unex.es>
2025-02-20 13:37:24 +0100

    filtro terminado

--- src/main/java/com/Backend/specification/PokemonSpecification.java
+++ src/main/java/com/Backend/specification/PokemonSpecification.java
@@ -1,5 +1,6 @@
 package com.Backend.specification;
 
+import java.util.HashMap;
 import java.util.Map;
 import org.springframework.data.jpa.domain.Specification;
 import com.Backend.Model.Pokemon;
@@ -25,8 +26,25 @@ public class PokemonSpecification implements Specification<Pokemon> {
 
             if (value != null && !value.toString().isEmpty()) {
                 switch (key) {
+                
+                	case "deck": // buscar por deck
+                		
+                		Map<String, String> deckMap = new HashMap<>();
+                		deckMap.put("A1", "A1");
+                		deckMap.put("PROMO", "PROMO");
+                		deckMap.put("A2", "A2");
+                		
+                		// deck = A1, PROMO, A2 --> buscar por setId
+                		if(deckMap.containsKey(value.toString())) {
+                			predicate = cb.and(predicate, cb.equal(root.get("setId"), deckMap.get(value.toString())));
+                		}
+                		// si es A1M, A1P, A1C, PROMOA1, PROMOA2, PROMOA3, A2D, A2P
+                		else {
+                	        predicate = cb.and(predicate, cb.like(root.get("deck"), "%" + value.toString() + "%"));
+                		}
+                		break;
                     case "pokemonName": //buscar por pokemonName
-                        predicate = cb.and(predicate, cb.equal(root.get("pokemonName"), value));
+                    	predicate = cb.and(predicate, cb.like(cb.lower(root.get("pokemonName")), "%" + value.toString().toLowerCase() + "%"));
                         break;
                     case "cardType": //buscar por cardType
                         predicate = cb.and(predicate, cb.equal(root.get("cardType"), value));
@@ -34,14 +52,39 @@ public class PokemonSpecification implements Specification<Pokemon> {
                     case "cardColor": //buscar por cardColor
                     	predicate = cb.and(predicate, cb.equal(root.get("cardColor"), value));
                     	break;
-                    case "rarity": //buscar por rarity (ocject a number)
+                    case "rarity": //buscar por rarity (string a number)
                     	predicate = cb.and(predicate, cb.equal(root.get("rarity"), Integer.parseInt(value.toString())));
                     	break;
+                    case "cardStage": //buscar por cardStage
+                    	predicate = cb.and(predicate, cb.equal(root.get("cardStage"), value));
+                    	break;
+                    case "weakness": //buscar por weakness
+                    	predicate = cb.and(predicate, cb.equal(root.get("weakness"), value));
+                    	break;
+                    case "retreatCost": //buscar por retreatCost (string a number)
+                    	predicate = cb.and(predicate, cb.equal(root.get("retreatCost"), Integer.parseInt(value.toString())));
+                    	break;
+                    case "sortBy":
+                    	if(value.toString().equals("pokemonName")) {
+                    		query.orderBy(cb.asc(root.get("pokemonName")));
+                    	}
+                    	else if(value.toString().equals("cardColor")) {
+                    		query.orderBy(cb.asc(root.get("cardColor")));                    		
+                    	}
+                    	else if(value.toString().equals("setId")) {
+                    		query.orderBy(cb.asc(root.get("setId")));                    		
+                    	}
+                    	else {
+                    		return cb.disjunction(); 
+                    	}
+                    	break;
+                    	
                     case "sortOrder": // Aplicar ordenación
                         String sortOrder = value.toString().toUpperCase();
-                        if ("ASC".equals(sortOrder)) {
+                        if (sortOrder.equals("ASC")) {
                             query.orderBy(cb.asc(root.get("setId"))); // Ordena por setId
-                        } else {
+                        } 
+                        else {
                             query.orderBy(cb.desc(root.get("setId")));
                         }
                         break;
