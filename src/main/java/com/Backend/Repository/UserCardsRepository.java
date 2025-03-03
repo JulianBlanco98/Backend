b56b40ead3b39df9fd6a8dc1d51a9b344bea6943
JulianBlanco98 <jblancoho@alumnos.unex.es>
2025-03-03 13:49:57 +0100

    avance de la bateria de test de cardUserCollection

--- src/main/java/com/Backend/Repository/UserCardsRepository.java
+++ src/main/java/com/Backend/Repository/UserCardsRepository.java
@@ -14,4 +14,7 @@ public interface UserCardsRepository extends JpaRepository<UserCards, Long>{
 	
 	boolean existsByCardUserCollectionAndCategory(CardUserCollection cardUserCollection, CardCategory category);
 	List<UserCards> findByCardUserCollectionAndCategory(CardUserCollection cardUserCollection, CardCategory category);
+	List<UserCards> findByCardUserCollection(CardUserCollection cardUserCollection);
+	void deleteByCardUserCollection(CardUserCollection cardUserCollection);
+
 }
--- src/test/java/com/Backend/BackendApplication_CardUserCollection_Test.java
+++ src/test/java/com/Backend/BackendApplication_CardUserCollection_Test.java
@@ -1,8 +1,11 @@
 package com.Backend;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,7 +13,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.Backend.Model.CardUserCollection;
+import com.Backend.Model.Pokemon;
+import com.Backend.Model.UserCards;
+import com.Backend.Model.UserCards.CardCategory;
+import com.Backend.Model.User.Role;
+import com.Backend.Repository.CardUserCollectionRepository;
+import com.Backend.Repository.PokemonRepository;
+import com.Backend.Repository.UserCardsRepository;
+import com.Backend.Repository.UserRepository;
 import com.Backend.Service.CardUserCollectionService;
+import com.Backend.Service.UserService;
+import com.Backend.dto.PokemonCollectionDTO;
+import com.Backend.dto.UserDTO;
+import com.Backend.mapper.UserMapper;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
@@ -19,24 +35,95 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootTest
 public class BackendApplication_CardUserCollection_Test {
 	
-	@Autowired
-	private CardUserCollectionService cardUserCollectionService;
+	// CardUserCollection y UserCards 
+	@Autowired private CardUserCollectionService cardUserCollectionService;
+	@Autowired private CardUserCollectionRepository cardUserCollectionRepository;
+	@Autowired private UserCardsRepository userCardsRepository;
+	
+	// Usuarios
+	@Autowired private UserService userService;
+	@Autowired private UserMapper userMapper;
+	@Autowired private UserRepository userRepository;
+	
+	// Pokemon
+	@Autowired private PokemonRepository pokemonRepository;
+	
+	// Objetos
+	private UserDTO userDTOTest;
+	private CardUserCollection collection_test;
 	
 	@BeforeEach
 	void setUp() {
+		
+		// Crear un usuario y registrarlo en la BD
+		this.userDTOTest = new UserDTO();
+		this.userDTOTest.setEmail("test@test.com");
+		this.userDTOTest.setUserName("userName_test");
+		this.userDTOTest.setLastName("lastName_test");
+		this.userDTOTest.setNickName("nickName_test");
+		this.userDTOTest.setDateOfBirth(new Date());
+		this.userDTOTest.setRol(Role.user);
+		this.userDTOTest.setPassword("1234");
+		this.userService.registerUser(userDTOTest);
+		
+		// Añadirle una tabla de colección y guardarlo en la BD
+		this.collection_test = new CardUserCollection();
+		collection_test.setUser(this.userMapper.userDTOToEntity(userDTOTest));
+		this.cardUserCollectionRepository.save(collection_test);
+		
+		// Voy a añadirle la expansión de Genetic y PROMO
+		List<Pokemon> geneticCollection = this.pokemonRepository.findBySetId("A1");
+		List<Pokemon> promoCollection = this.pokemonRepository.findBySetId("PROMO");
+		
+		List<UserCards> userCards1 = geneticCollection.stream().map(pokemon -> {
+			UserCards card = new UserCards();
+			card.setCardUserCollection(collection_test);
+			card.setPokemon(pokemon);
+			card.setCategory(CardCategory.Genetic);
+			card.setHasTheCard(false);
+			return card;
+		}).toList();
+		this.userCardsRepository.saveAll(userCards1);
+		
+		List<UserCards> userCards2 = promoCollection.stream().map(pokemon -> {
+			UserCards card = new UserCards();
+			card.setCardUserCollection(collection_test);
+			card.setPokemon(pokemon);
+			card.setCategory(CardCategory.PROMO);
+			card.setHasTheCard(false);
+			return card;
+		}).toList();		
+		this.userCardsRepository.saveAll(userCards2);
+		
+		
+//		List<UserCards> allCards = this.userCardsRepository.findByCardUserCollection(collection_test);
+//		log.info("Cartas guardadas en BD: {}", allCards.size());
 		
 	}
 	
 	@AfterEach
 	void tearDown() {
 		
+		if(collection_test != null) {
+			
+			// Borrar las userCards dependiendo del parámetro CardUserCollection
+			List<UserCards> userCards = this.userCardsRepository.findByCardUserCollection(collection_test);
+			this.userCardsRepository.deleteAll(userCards);
+			
+			// Borrar el cardUserCollection
+			this.cardUserCollectionRepository.delete(collection_test);
+			
+		}
+		
+		// Borrar el usuario
+		this.userRepository.delete(this.userMapper.userDTOToEntity(userDTOTest));
 	}
 	
 	@Test
 	void test_isCollectionInitialized_IncorrectGmail() {
 		
 		assertThrows(EntityNotFoundException.class, () -> {
-			this.cardUserCollectionService.isCollectionInitialized("random@random.com", "Genetic");
+			this.cardUserCollectionService.isCollectionInitialized("test2@test.com", "Genetic");
 		});
 		
 	}
@@ -44,14 +131,38 @@ public class BackendApplication_CardUserCollection_Test {
 	void test_isCollectionInitialized_IncorrectExpansion() {
 		
 		assertThrows(IllegalArgumentException.class, () -> {
-			this.cardUserCollectionService.isCollectionInitialized("prueba@gmail.com", "sdsdsdsd");
+			this.cardUserCollectionService.isCollectionInitialized("test@test.com", "sdsdsdsd");
 		});
 		
 	}
 	@Test
 	void test_isCollectionInitialized() {
 		
-		assertTrue(this.cardUserCollectionService.isCollectionInitialized("prueba@gmail.com", "Genetic"));
+		assertTrue(this.cardUserCollectionService.isCollectionInitialized("test@test.com", "Genetic"));
+		assertTrue(this.cardUserCollectionService.isCollectionInitialized("test@test.com", "PROMO"));
 	}
+	
+	@Test
+	void test_initCollection_incorrectExpansion() {
+		
+		assertThrows(IllegalArgumentException.class, () -> {
+			this.cardUserCollectionService.initializeCollection("test@test.com", "adasdasda");
+		});
+	}
+	@Test
+	void test_initCollection() {
+		
+		this.cardUserCollectionService.initializeCollection("test@test.com", "Mythical");
+		
+		List<PokemonCollectionDTO> mythicalCards = this.cardUserCollectionService.getUserExpansionCards("test@test.com", "Mythical");
+		
+		assertNotNull(mythicalCards);
+		assertTrue(mythicalCards.size() == 86);
+		
+		
+		
+	}
+	
+	
 
 }
