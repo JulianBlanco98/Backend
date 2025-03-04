3e478edbad422773f35c44afa55090e440b11b24
JulianBlanco98 <jblancoho@alumnos.unex.es>
2025-03-05 00:19:30 +0100

    mejorastest

--- src/test/java/com/backend/BackendAppliactionUserTest.java
+++ src/test/java/com/backend/BackendAppliactionUserTest.java
@@ -1,9 +1,11 @@
 package com.backend;
 
+import com.backend.dto.UserDTO;
 import com.backend.mapper.UserMapper;
 import com.backend.model.User;
 import com.backend.repository.UserRepository;
 import com.backend.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -13,8 +15,7 @@ import org.springframework.test.context.TestPropertySource;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest()
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -34,11 +35,21 @@ class BackendAppliactionUserTest {
 
     @Test
     void findByEmail_Correct() {
-        System.out.println("Cantidad de usuarios en BD: " + userRepository.count()); // Verifica cuántos registros hay
-        Optional<User> user = this.userRepository.findByEmail("test1@test.com");
-        assertTrue(user.isPresent(), "El usuario test1 debería estar en la BD");
-        assertEquals("test1@test.com", user.get().getEmail());
-        assertEquals(4, this.userRepository.count(), "Tiene que haber 4 usuarios");
+        String correctEmail = "test1@test.com";
+        UserDTO user = this.userService.findByEmail(correctEmail);
+        assertEquals(correctEmail, user.getEmail());
+        assertEquals("user1", user.getUserName());
+        assertEquals("last1", user.getLastName());
+        assertEquals("nick1", user.getNickName());
+
+    }
+
+    @Test
+    void findByEmail_NotFound() {
+        String wrongEmail = "noEmailTest@test.com";
+        assertThrows(EntityNotFoundException.class, () -> {
+           this.userService.findByEmail(wrongEmail);
+        });
 
     }
 
