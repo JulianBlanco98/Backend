b7b0521335d58478671c6a0e6b649f523c97642c
JulianBlanco98 <jblancoho@alumnos.unex.es>
2025-03-05 11:26:22 +0100

    creacion de la manera correcta los test

--- src/test/java/com/backend/service/UserServiceImplTest.java
+++ src/test/java/com/backend/service/UserServiceImplTest.java
@@ -1,10 +1,6 @@
-package com.backend;
+package com.backend.service;
 
 import com.backend.dto.UserDTO;
-import com.backend.mapper.UserMapper;
-import com.backend.model.User;
-import com.backend.repository.UserRepository;
-import com.backend.service.UserService;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,44 +9,45 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
-import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest()
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.properties")
-class BackendAppliactionUserTest {
+class UserServiceImplTest {
 
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private UserRepository userRepository;
+    @Test
+    void findByEmail_NotFound() {
+        String wrongEmail = "noEmailTest@test.com";
+        assertThrows(EntityNotFoundException.class, () -> {
+            this.userService.findByEmail(wrongEmail);
+        });
 
-    @Autowired
-    UserMapper userMapper;
-
+    }
 
     @Test
-    void findByEmail_Correct() {
+    void findByEmail() {
         String correctEmail = "test1@test.com";
         UserDTO user = this.userService.findByEmail(correctEmail);
         assertEquals(correctEmail, user.getEmail());
         assertEquals("user1", user.getUserName());
         assertEquals("last1", user.getLastName());
         assertEquals("nick1", user.getNickName());
+    }
 
+
+
+    /*@Test
+    void registerUser() {
     }
 
     @Test
-    void findByEmail_NotFound() {
-        String wrongEmail = "noEmailTest@test.com";
-        assertThrows(EntityNotFoundException.class, () -> {
-           this.userService.findByEmail(wrongEmail);
-        });
-
-    }
-
+    void loginUser() {
+    }*/
 }
