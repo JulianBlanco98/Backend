0af415433f4448cc9d8ad2e5f495f1fccea41757
JulianBlanco98 <jblancoho@alumnos.unex.es>
2025-03-07 12:39:44 +0100

    creada la bateria de test de pokemonService (pokemonSpeccification tambien)
    Tengo que ver como hacer el caso de sortOrderDesc

--- src/test/java/com/backend/service/PokemonServiceImplTest.java
+++ src/test/java/com/backend/service/PokemonServiceImplTest.java
@@ -1,9 +1,8 @@
 package com.backend.service;
 
+import com.backend.dto.PokemonCollectionDTO;
 import com.backend.dto.PokemonDTO;
 import jakarta.persistence.EntityNotFoundException;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -13,7 +12,9 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,24 +28,12 @@ class PokemonServiceImplTest {
     @Autowired
     private PokemonService pokemonService;
 
-    @BeforeEach
-    void setUp() {
-
-
-    }
-
-    @AfterEach
-    void tearDown()  {
-
-    }
-
-
     @Transactional
     @Test
     void findAll() {
-        List<PokemonDTO> pokemon = this.pokemonService.findAll();
+        final List<PokemonDTO> pokemon = this.pokemonService.findAll();
         assertNotNull(pokemon, "La lista no puede ser nula");
-        assertTrue(pokemon.size() > 0, "La lista no debería estar vacía");
+        assertFalse(pokemon.isEmpty(), "La lista no debería estar vacía");
         assertEquals("A1-004", pokemon.get(0).getIdPokemon(), "El primer pokemon tiene id: A1-004");
         assertEquals("A1-007", pokemon.get(1).getIdPokemon(), "El primer pokemon tiene id: A1-007");
     }
@@ -52,7 +41,7 @@ class PokemonServiceImplTest {
 
     @Test
     void findByPokemonName_IncorrectName() {
-        String nameIncorrect = "Incorrect";
+        final String nameIncorrect = "Incorrect";
         assertThrows(EntityNotFoundException.class, () -> {
             this.pokemonService.findByPokemonName(nameIncorrect);
         });
@@ -61,8 +50,8 @@ class PokemonServiceImplTest {
 
     @Test
     void findByPokemonName() {
-        String correctName = "Venusaur ex";
-        List<PokemonDTO> correct = this.pokemonService.findByPokemonName(correctName);
+        final String correctName = "Venusaur ex";
+        final List<PokemonDTO> correct = this.pokemonService.findByPokemonName(correctName);
         assertNotNull(correct, "Tiene que encontrar uno por el nombre de Venusaur Ex");
         assertEquals(1, correct.size(), "Hay solo un Venusaur Ex");
         assertEquals("Grass", correct.get(0).getCardColor(), "El tipo es Grass");
@@ -71,7 +60,7 @@ class PokemonServiceImplTest {
 
     @Test
     void findByIdPokemon_IncorrectId() {
-        String idIncorrect = "A1-287";
+        final String idIncorrect = "A1-287";
         assertThrows(EntityNotFoundException.class, () -> {
             this.pokemonService.findByIdPokemon(idIncorrect);
         });
@@ -79,19 +68,187 @@ class PokemonServiceImplTest {
 
     @Test
     void findByIdPokemon() {
-        String correctId = "A1-004";
-        PokemonDTO expected = this.pokemonService.findByIdPokemon(correctId);
+        final String correctId = "A1-004";
+        final PokemonDTO expected = this.pokemonService.findByIdPokemon(correctId);
         assertNotNull(expected, "Tiene que encontrar a Venusaur Ex");
         assertEquals("Venusaur ex", expected.getPokemonName(), "El nombre tiene que ser Venusaur Ex");
         assertEquals("Grass", expected.getCardColor(), "El tipo es Grass");
+
+        final List<PokemonDTO.AttackDTO> attackDTOS = expected.getAttack();
+        assertNotNull(attackDTOS);
+        assertEquals(2, attackDTOS.size(), "Venusaur ex tiene 2 ataques");
+        assertNull(attackDTOS.get(0).getEffect(), "Este ataque no tiene efecto. Deber ser NULL");
     }
 
-    /*
     @Test
-    void findByFilters() {
+    void findByFilters_PokemonsNotFound() {
+        Map<String, Object> filterPokemonName = new HashMap<>();
+        filterPokemonName.put("pokemonName", "Charizard");
+        assertThrows(EntityNotFoundException.class, () -> {
+            this.pokemonService.findByFilters(filterPokemonName);
+        });
+
+    }
+
+    @Transactional
+    @Test
+    void findByFilters_DeckToSetId() {
+        Map<String, Object> filterPokemon = new HashMap<>();
+        filterPokemon.put("deck", "A1a");
+
+        final List<PokemonDTO> expected = this.pokemonService.findByFilters(filterPokemon);
+        assertNotNull(expected, "Debería encontrar a 2 pokemons");
+        assertEquals(2, expected.size(), "Hay 2 pokemon de este deck A1a");
+        assertEquals("A1a-078", expected.get(0).getIdPokemon(), "IdPokemon esperado: A1a-078");
+        assertEquals("A1a-086", expected.get(1).getIdPokemon(), "IdPokemon esperado: A1a-086");
+    }
+
+    @Transactional
+    @Test
+    void findByFilters_Deck() {
+        Map<String, Object> filterPokemon = new HashMap<>();
+        filterPokemon.put("deck", "A1P");
+
+        final List<PokemonDTO> expected = this.pokemonService.findByFilters(filterPokemon);
+        assertNotNull(expected, "Debe encontrar 1");
+        assertEquals(1, expected.size(), "Encuentra un total de 1 pokemon");
+        assertEquals("A1-007", expected.get(0).getIdPokemon(), "Id esperado: A1-007");
+    }
+
+    @Transactional
+    @Test
+    void findByFilters_PokemonName() {
+        Map<String, Object> filterPokemonName = new HashMap<>();
+        filterPokemonName.put("pokemonName", "Butterfree");
+
+        final List<PokemonDTO> expected = this.pokemonService.findByFilters(filterPokemonName);
+        assertNotNull(expected, "Existe Butterfree");
+        assertEquals("A1-007", expected.get(0).getIdPokemon(), "El idPokemon es: A1-007");
+        assertEquals("Genetic Apex", expected.get(0).getCollectionPocket(), "la colección es: Genetic Apex");
+
+    }
+
+    @Transactional
+    @Test
+    void findByFilters_CardColorAndCardTypeAndCardStage() {
+        Map<String, Object> filterPokemon = new HashMap<>();
+        filterPokemon.put("cardColor", "Psychic");
+        filterPokemon.put("cardType", "Pokemon");
+        filterPokemon.put("cardStage", "Basic");
+
+        final List<PokemonDTO> expected = this.pokemonService.findByFilters(filterPokemon);
+        assertNotNull(expected, "Debería encontrar a Mew Ex");
+        assertEquals("Mew ex", expected.get(0).getPokemonName(), "PokemonName: Mew ex");
+    }
+
+    @Transactional
+    @Test
+    void findByFilters_RarityAndWeaknessAndRetreatCost() {
+        Map<String, Object> filterPokemon = new HashMap<>();
+        filterPokemon.put("rarity", 7);
+        filterPokemon.put("weakness", "Lightning");
+        filterPokemon.put("retreatCost", 1);
+
+        final List<PokemonDTO> expected = this.pokemonService.findByFilters(filterPokemon);
+        assertNotNull(expected, "Debería encontrar a Aerodactyl ex");
+        assertEquals("Aerodactyl ex", expected.get(0).getPokemonName(), "PokemonName: Mew ex");
+    }
+
+    @Transactional
+    @Test
+    void findByFilters_SortByPokemonName() {
+        Map<String, Object> filterPokemon = new HashMap<>();
+        filterPokemon.put("sortBy", "pokemonName");
+
+        final List<PokemonDTO> expected = this.pokemonService.findByFilters(filterPokemon);
+        assertNotNull(expected, "Tienen que estar los 4 pokemons");
+        assertEquals("A1a-078", expected.get(0).getIdPokemon(), "1: Aerodactyl");
+        assertEquals("A1-007", expected.get(1).getIdPokemon(), "2: Butterfree");
+        assertEquals("A1a-086", expected.get(2).getIdPokemon(), "3: Mew");
+        assertEquals("A1-004", expected.get(3).getIdPokemon(), "4: Venusaur");
+
+    }
+
+    @Transactional
+    @Test
+    void findByFilters_SortByCardColor() {
+        Map<String, Object> filterPokemon = new HashMap<>();
+        filterPokemon.put("sortBy", "cardColor");
+
+        final List<PokemonDTO> expected = this.pokemonService.findByFilters(filterPokemon);
+        assertNotNull(expected, "Tienen que estar los 4 pokemons");
+        assertEquals("A1a-078", expected.get(0).getIdPokemon(), "1: Fightning");
+        assertEquals("A1-004", expected.get(1).getIdPokemon(), "2: Grass");
+        assertEquals("A1-007", expected.get(2).getIdPokemon(), "3: Grass");
+        assertEquals("A1a-086", expected.get(3).getIdPokemon(), "4: Lightning");
+
+    }
+
+    @Transactional
+    @Test
+    void findByFilters_SortBySetId() {
+        Map<String, Object> filterPokemon = new HashMap<>();
+        filterPokemon.put("sortBy", "setId");
+
+        final List<PokemonDTO> expected = this.pokemonService.findByFilters(filterPokemon);
+        assertNotNull(expected, "Tienen que estar los 4 pokemons");
+        assertEquals("A1-004", expected.get(0).getIdPokemon(), "1 Id: A1-004");
+        assertEquals("A1-007", expected.get(1).getIdPokemon(), "2 Id: A1-007");
+        assertEquals("A1a-078", expected.get(2).getIdPokemon(), "3 Id: A1a-078");
+        assertEquals("A1a-086", expected.get(3).getIdPokemon(), "4 Id: A1a-086");
+
+    }
+
+    @Transactional
+    @Test
+    void findByFilters_SortOrderASC() {
+        Map<String, Object> filterPokemon = new HashMap<>();
+        filterPokemon.put("sortOrder", "ASC");
+
+        final List<PokemonDTO> expected = this.pokemonService.findByFilters(filterPokemon);
+        assertNotNull(expected, "Tienen que estar los 4 pokemons");
+        assertEquals("A1-004", expected.get(0).getIdPokemon(), "1 Id: A1-004");
+        assertEquals("A1-007", expected.get(1).getIdPokemon(), "2 Id: A1-007");
+        assertEquals("A1a-078", expected.get(2).getIdPokemon(), "3 Id: A1a-078");
+        assertEquals("A1a-086", expected.get(3).getIdPokemon(), "4 Id: A1a-086");
+
+    }
+
+    @Transactional
+    @Test
+    void findByFilters_SortOrderDESC() {
+        Map<String, Object> filterPokemon = new HashMap<>();
+        filterPokemon.put("sortOrder", "DESC");
+
+        final List<PokemonDTO> expected = this.pokemonService.findByFilters(filterPokemon);
+        for (PokemonDTO pokemonDTO : expected) {
+            System.out.println("Id pokemon orden: " + pokemonDTO.getIdPokemon());
+        }
+
+        /* Cambiar el Specification para que el orden sea descendiente */
+        assertNotNull(expected, "Tienen que estar los 4 pokemons");
+        assertEquals("A1a-078", expected.get(0).getIdPokemon(), "1 Id: A1a-086");
+        assertEquals("A1a-086", expected.get(1).getIdPokemon(), "2 Id: A1a-078");
+        assertEquals("A1-004", expected.get(2).getIdPokemon(), "3 Id: A1-007");
+        assertEquals("A1-007", expected.get(3).getIdPokemon(), "4 Id: A1-004");
+
+    }
+
+
+    @Test
+    void findByIdPokemonCollection_IncorrectId() {
+        final String idIncorrect = "S1-001";
+        assertThrows(EntityNotFoundException.class, () -> {
+           this.pokemonService.findByIdPokemonCollection(idIncorrect);
+        });
     }
 
     @Test
     void findByIdPokemonCollection() {
-    }*/
+        final String idCorrect = "A1-007";
+
+        final PokemonCollectionDTO expected = this.pokemonService.findByIdPokemonCollection(idCorrect);
+        assertNotNull(expected, "Tiene que encontrar uno");
+        assertEquals(idCorrect, expected.getIdPokemon(), "El id es: A1-007");
+    }
 }
--- src/test/resources/sql/pokemon_data.sql
+++ src/test/resources/sql/pokemon_data.sql
@@ -1,14 +1,20 @@
--- Primero, añado los ataques y las habilidades
+-- Primero, añado los ataques
 INSERT INTO attack (id, info, effect)
 VALUES
-(1, '{GGCC} Mega Drain 80', 'Heal 30 damage from this Pokémon'),
-(2, '{FC} Land Crush 80', NULL),
-(3, '{CCC} Genome Hacking', 'Choose 1 of your opponents Active Pokémons attacks and use it as this attack.');
+(1, '{GCC} Razor Leaf 60', NULL), -- Venusaur Ex
+(2, '{GGCC} Mega Drain 80', 'Heal 30 damage from this Pokémon'), -- Venusaur Ex
+(3, '{GCC} Gust 60', NULL), -- Butterfree
+(4, '{FC} Land Crush 80', NULL), -- Aerodactyl Ex
+(5, '{CCC} Genome Hacking', 'Choose 1 of your opponents Active Pokémons attacks and use it as this attack.'); -- Mew Ex
+
+-- Luego, las habilidades
 
 INSERT INTO ability (id, effect, info)
 VALUES
 (1, 'Once during your turn, you may heal 20 damage from each of your Pokémon.', 'Powder Heal'),
 (2, 'Your opponent cant play any Pokémon from their hand to evolve their Active Pokémon.', 'Primeral Law');
+
+-- Luego, añado los pokemon
 
 INSERT INTO pokemons (idPokemon, setId, idNumber, pokemonName, collectionPocket, deck, cardType, cardColor, rarity, cardStage, retreatCost, imageUrl, hpPokemon, textCards, weakness)
 VALUES
@@ -16,3 +22,20 @@ VALUES
 ('A1-007', 'A1', 7, 'Butterfree', 'Genetic Apex', 'A1P', 'Pokemon', 'Grass', 4, 'Stage 2', 1, 'https://res.cloudinary.com/tgcpokemon/image/upload/v1736255346/Genetic/A1-007.webp', 120, NULL, 'Fire'),
 ('A1a-078', 'A1a', 78, 'Aerodactyl ex', 'Mythical Island', 'Promo', 'Pokemon', 'Fighting', 7, 'Stage 1', 1, 'https://res.cloudinary.com/tgcpokemon/image/upload/v1736255346/Island/A1a-078.webp', 140, NULL, 'Lightning'),
 ('A1a-086', 'A1a', 86, 'Mew ex', 'Mythical Island', 'Promo', 'Pokemon', 'Psychic', 10, 'Basic', 1, 'https://res.cloudinary.com/tgcpokemon/image/upload/v1736255346/Island/A1a-086.webp', 130, NULL, 'Darkness');
+
+-- y por último, relleno ambas tablas intermedias
+
+INSERT INTO pokemon_attack (pokemon_id, attack_id)
+VALUES
+('A1-004', 1), -- Venusaur Ex
+('A1-004', 2), -- Venusaur Ex
+('A1-007', 3), -- Butterfree
+('A1a-078', 4), -- Aerodactyl Ex
+('A1a-086', 5); -- Mew Ex
+
+INSERT INTO pokemon_ability (pokemon_id, ability_id)
+VALUES
+('A1-007', 1), -- Butterfree
+('A1a-078', 2); -- Aerodactyl Ex
+
+
