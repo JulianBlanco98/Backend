9a6088a6d3231b99eaca277229fa97e28da1c81a
JulianBlanco98 <jblancoho@alumnos.unex.es>
2025-03-18 14:24:16 +0100

    bateria de test para la clase main

---
+++ src/test/java/com/backend/BackendApplicationTest.java
@@ -0,0 +1,21 @@
+package com.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
+class BackendApplicationTest {
+
+    @Test
+    void main() {
+        BackendApplication.main(new String[]{});
+    }
+}
